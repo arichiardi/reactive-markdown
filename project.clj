0563d729e6a07586b5fa4b2b2fb7b97103827f66
Andrea Richiardi <a.richiardi.work@gmail.com>
2015-10-06 19:28:40 -0700

    Bump stuff in project.clj and add demo harness.

--- project.clj
+++ project.clj
@@ -1,29 +1,18 @@
 (defproject reactive-markdown "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.122"]
-                 [reagent "0.5.0"]]
-
-  :source-paths ["src/clj"]
-
-  :plugins [[lein-cljsbuild "1.0.6"]
-            [lein-figwheel "0.3.9" :exclusions [cider/cider-nrepl]]  ]
-
-  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"  ]
-
-  :cljsbuild {:builds [{:id "dev"
-                        :source-paths ["src/cljs"]
-
-                        :figwheel {:on-jsload "reactive-markdown.core/main"}
-
-                        :compiler {:main reactive-markdown.core
-                                   :output-to "resources/public/js/compiled/app.js"
-                                   :output-dir "resources/public/js/compiled/out"
-                                   :asset-path "js/compiled/out"
-                                   :source-map-timestamp true}}
-
-                       {:id "min"
-                        :source-paths ["src/cljs"]
-                        :compiler {:main reactive-markdown.core
-                                   :output-to "resources/public/js/compiled/app.js"
-                                   :optimizations :advanced
-                                   :pretty-print false}}]})
+                 [reagent "0.5.1"]
+                 [cljsjs/showdown "0.4.0-1"]]
+  :plugins [[lein-cljsbuild "1.1.0"]
+            [lein-figwheel "0.4.0" :exclusions [cider/cider-nrepl]]  ]
+  :clean-targets ^{:protect false} ["resources/public/reactive-markdown-demo/js/compiled" "target"]
+  :scm {:name "git"
+        :url "https://github.com/arichiardi/reactive-markdown" }
+  :cljsbuild {:builds [{:id "demo"
+                        :source-paths ["src/cljs" "demo-src/cljs"]
+                        :figwheel {:on-jsload "reactive-markdown.demo/main"}
+                        :compiler {:main reactive-markdown.demo
+                                   :output-to "resources/public/reactive-markdown-demo/js/compiled/reactive-markdown-demo.js"
+                                   :output-dir "resources/public/reactive-markdown-demo/js/compiled/out"
+                                   :asset-path "reactive-markdown-demo/js/compiled/out"
+                                   :source-map-timestamp true}}]})
